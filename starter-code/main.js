62131735030fb357af53511a13e4d6117b4ba2aa
John Christie <johnchristie@172-30-48-77.DYNAPOOL.NYU.EDU>
2016-06-30 13:14:33 -0400

    created game alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+}
+
+if (cardTwo === cardThree) {
+	alert("You found a match!")
+} else {alert ("Sorry, try again.")}
